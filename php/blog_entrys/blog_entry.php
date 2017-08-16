7d0c070612160653337a291b100ba66107f52547
FloMarks <f.marks95@web.de>
2017-08-16 11:42:21 +0200

    Daten von Datenbank werden nun im Beitrag Fenster angezeigt

--- php/blog_entrys/blog_entry.php
+++ php/blog_entrys/blog_entry.php
@@ -39,26 +39,26 @@ session_start();
     </header>
 
       <?php
-            
+            //write mysql data in html fields
             $pdo = new PDO('mysql:host=localhost;dbname=login_system', 'root', '');
             $sql = "SELECT * FROM blog_entry WHERE title='sd'";
       
             foreach ($pdo->query($sql) as $row);
       ?>
       
+      
 	<div class = "opening-picture">
-        <h1 value="<?php echo $row['title'] ;?>"></h1>
+        <h1><?php echo $row['title'] ;?></h1>
   </div>
 
   <div class = "blog-info">
-    <p>Erstellt von: flo_marks</p>
-    <p>Datum: 17.08.2017</p>
+    <p><?php echo $row['user_name'];?></p>
+    <p><?php echo $row['date'] ;?></p>
   </div>
 
   <div class = "blog-content">
     <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+     <?php echo $row['content'] ;?>
     </p>
   </div>
   
