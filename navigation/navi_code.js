736c014c53dd7133529099d1d4ad749a8dd3b3d5
Spacket <valerij.stelmach@live.de>
2017-08-31 14:28:51 +0200

    js minimiert

--- navigation/navi_code.js
+++ navigation/navi_code.js
@@ -1,8 +1,1 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+function myFunction(){var x=document.getElementById("myTopnav");if(x.className==="topnav"){x.className+=" responsive";}else{x.className="topnav";}}
--- profil/ajax_profil.js
+++ profil/ajax_profil.js
@@ -1,10 +1,1 @@
-var blogCount = 0; 
-
-function nextEntry() {
-    //jQuery          
-          blogCount = blogCount + 1;
-          $("#entry").load("includes/user_profile-inc.php", {
-            cur: blogCount
-          });
-    
-    }
+var blogCount=0;function nextEntry(){blogCount=blogCount+1;$("#entry").load("includes/user_profile-inc.php",{cur:blogCount});}
